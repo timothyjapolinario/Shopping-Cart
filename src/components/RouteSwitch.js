4c16fc1dba49091fd8da4c4bf12d340ede9a5347
JayJay <timothyjapolinario@gmail.com>
2022-12-20 12:44:06 +0800

    Add state for cart

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -3,14 +3,27 @@ import App from "../App";
 import "../styles/RouteSwitch.css";
 import Shop from "../pages/Shop";
 import ProductInfo from "../pages/ProductInfo";
+import { useEffect, useState } from "react";
 const RouteSwitch = () => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  const addToCart = (item) => {
+    setCart(cart.concat(item));
+  };
   return (
     <div className="RouteSwitch">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/:id" element={<ProductInfo />} />
+          <Route path="/" element={<App cart={cart} />} />
+          <Route path="/shop" element={<Shop cart={cart} />} />
+          <Route
+            path="/shop/:id"
+            element={<ProductInfo cart={cart} addItem={addToCart} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/ProductInfo.js
+++ src/pages/ProductInfo.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 import getBreads from "../modules/Breads";
 import getCoffees from "../modules/Coffee";
+import Header from "../components/Header";
+import "../styles/ProductInfo.css";
 const fetchProduct = (id) => {
   const idInt = parseInt(id);
   const allProducts = [...getBreads(), ...getCoffees()];
@@ -12,7 +14,7 @@ const fetchProduct = (id) => {
   return prod;
 };
 
-const ProductInfo = () => {
+const ProductInfo = ({ cart, addItem }) => {
   const params = useParams();
   const [product, setProduct] = useState(fetchProduct(params.id));
   console.log(fetchProduct(params.id));
@@ -21,8 +23,29 @@ const ProductInfo = () => {
   }, [product]);
 
   return (
-    <div>
-      <h1>{product.name}</h1>
+    <div className="ProductInfo  bg-pattern">
+      <Header />
+      <div className="content-product-info">
+        <div>
+          <img
+            id="productInfoImage"
+            src={product.imageURL}
+            alt={product.name + " image"}
+          />
+        </div>
+        <div className="product-info-text">
+          <h1 id="pi-product-name">{product.name}</h1>
+          <div
+            id="add-to-cart-button"
+            onClick={() => {
+              addItem(product);
+            }}
+          >
+            <div>Add To Cart</div>
+            <div>{"$" + product.price}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
